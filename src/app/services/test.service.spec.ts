43d2bc33537f8de6edfab7118aeb6327fb81f354
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-07-14 19:02:47 +0500

    .

---
+++ src/app/services/test.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TestService } from './test.service';
+
+describe('TestService', () => {
+  let service: TestService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TestService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('#getName: Service nomini qaytarishi kerak', () => {
+    expect(service.getName()).toBe('Test');
+  });
+
+  it('#getNameObsarvable: Observable tipida nameni qaytarishi kerak', (done: DoneFn) => {
+    service.getNameObservable().subscribe((name) => {
+      expect(name).toBe('Test');
+      done();
+    });
+  });
+});
--- src/app/tests/test/test.component.spec.ts
+++ src/app/tests/test/test.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PipesComponent } from './pipes.component';
+import { TestComponent } from './test.component';
 
-describe('PipesComponent', () => {
-  let component: PipesComponent;
-  let fixture: ComponentFixture<PipesComponent>;
+describe('TestComponent', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PipesComponent]
+      declarations: [TestComponent]
     });
-    fixture = TestBed.createComponent(PipesComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
